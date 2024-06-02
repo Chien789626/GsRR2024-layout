7a6b911a08b62b4b6376ef61e2f06676fe72cedc
Chien789626 <grf8ctoc_flbo@outlook.com>
2024-06-02 17:49:41 +0900

    字幕　スクロール速度調整

--- src/graphics/components/textInfo.vue
+++ src/graphics/components/textInfo.vue
@@ -17,19 +17,19 @@
   bottom: 0;
   font-size: 34px;
   font-family: 'KomorebiGothic';
-  border: 1px solid black;
+  /*border: 1px solid black;*/
 }
 .loop {
   width: 100%;
   position: absolute;
   bottom: 5px;
-  animation: loop 10s linear infinite;
-  border: 1px solid red;
+  animation: loop 120s linear infinite;
+  /*border: 1px solid red;*/
 }
 
 .loop2 {
   position: absolute;
-  animation: loop 10s -5s linear infinite;
+  animation: loop 120s -60s linear infinite;
 }
 
 @keyframes loop {
